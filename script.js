7f5af3916871f4429c9d6d9d1c9dade6aed5d6c5
djjean <djjean90@gmail.com>
2021-12-21 09:19:11 +0200

    final

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 const result = document.querySelector('#result');
 const filter = document.querySelector('#filter');
+
+const listItem = [];
+
+filter.addEventListener('input', (e) => filterData(e.target.value));
+
+getData();
+
+async function getData() {
+  const res = await fetch('https://randomuser.me/api?results=50');
+  const { results } = await res.json();
+
+  result.innerHTML = '';
+
+  results.forEach((user) => {
+    const li = document.createElement('li');
+
+    listItem.push(li);
+
+    li.innerHTML = `
+    <img src="${user.picture.large}" alt="${user.name.first}"
+    <div class="user-info">
+        <h4>${user.name.first} ${user.name.last}</h4>
+        <p>${user.location.city} , ${user.location.country}</p>
+    </div>
+    `;
+
+    result.appendChild(li);
+  });
+}
+
+function filterData(searchTerm) {
+  console.log(listItem);
+  listItem.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove('hide');
+    } else {
+      item.classList.add('hide');
+    }
+  });
+}
